0132ad5952cc9365b9ad888a547b0a66376880a0
westbanana <93942346+westbanana@users.noreply.github.com>
2023-11-04 23:48:14 +0200

    1. coment;

--- src/widgets/Sidebar/ui/SIdebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/SIdebar/Sidebar.tsx
@@ -12,21 +12,21 @@ interface SidebarProps {
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
 
-    const onToggle = () => {
-        setCollapsed((prev) => !prev);
-    };
+    // const onToggle = () => {
+    //     setCollapsed((prev) => !prev);
+    // };
 
     return (
         <div
             data-testid="sidebar"
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            <Button
-                data-testid="sidebar-toggle"
-                onClick={onToggle}
-            >
-                toggle
-            </Button>
+            {/* <Button */}
+            {/*    data-testid="sidebar-toggle" */}
+            {/*    onClick={onToggle} */}
+            {/* > */}
+            {/*    toggle */}
+            {/* </Button> */}
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
