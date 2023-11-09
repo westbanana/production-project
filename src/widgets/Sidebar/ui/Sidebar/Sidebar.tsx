1db55f165b38e41c33f7414a3e0b000c64359a88
westbanana <93942346+westbanana@users.noreply.github.com>
2023-11-10 00:06:46 +0200

    1. Fixed test;

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -15,7 +15,7 @@ interface SidebarProps {
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-    const [collapsed, setCollapsed] = useState(true);
+    const [collapsed, setCollapsed] = useState(false);
     const { t } = useTranslation();
     const onToggle = () => {
         setCollapsed((prev) => !prev);
