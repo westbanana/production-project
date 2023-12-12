9a7637ae78efa4e233f43e409fd0d9759819fc58
westbanana <93942346+westbanana@users.noreply.github.com>
2023-12-12 17:40:49 +0200

    Deleted test Input

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation();
-    const [value, setValue] = useState('');
-
-    const onChange = (val: string) => {
-        setValue(val);
-    };
 
     return (
         <div>
             {t('Головна сторінка')}
-            <Input
-                value={value}
-                onChange={onChange}
-                placeholder="Type text"
-            />
-            {value}
         </div>
     );
 };
